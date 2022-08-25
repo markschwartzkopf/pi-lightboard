61be0e82cb74572131653e8a38862ef68f6aa548
Mark Schwartzkopf <mark@schwartzkopf.org>
2022-08-24 19:07:38 -0500

    inc

--- src/server/types/index.d.ts
+++ src/server/types/index.d.ts
@@ -1,13 +1,17 @@
 type objectType = 'fixture' | 'group';
-interface Settings {
+type GroupVal = { t: 'g'; v: number; min: number; max: number };
+type FixtureVal = { t: 'f'; v: number };
+type ObjectVal = GroupVal | FixtureVal
+
+type SettingType = {
+	std: { type: 'std'; val: ObjectVal }; //range from 0 - 1
+	color: { type: 'color'; h: ObjectVal, s: ObjectVal }; //hue range from 0 - 2Pi, saturation range from 0 - 1
+};
+
+type Settings = {
 	//[K: string]: settingType[keyof settingType]; //But...
 	//Forcing all properties to a type by name
 	//To prevent intersection of two same-named properties with different types
 	color?: SettingType['color'];
 	value?: SettingType['std'];
-}
-
-interface SettingType {
-	std: { type: 'std'; val: number; minVal: number; maxVal: number }; //range from 0 - 1
-	color: { type: 'color'; h: number; minH: number; maxH: number; s: number; minS: number; maxS: number }; //hue range from 0 - 2Pi, saturation range from 0 - 1
-}
+};
